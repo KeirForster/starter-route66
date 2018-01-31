65019f763a819ccc32c3306451db826b66a82fd9
ZhiyuanEric <linzhiyuaneric@yahoo.com>
2018-01-30 17:15:48 -0800

    completed Bravo

--- application/controllers/Bravo/Welcome.php
+++ application/controllers/Bravo/Welcome.php
@@ -3,12 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends Application
 {
-	/**
-	 * Homepage for our app
-	 */
+
 	public function index()
 	{
-		// this is the view we want shown
 		$this->show(2);
 	}
 
